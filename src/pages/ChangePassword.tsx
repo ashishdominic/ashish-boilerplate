ea921124d656da0753a630dbcc0cf37d251b778f
Ashish Dominic <ashishdominic265@gmail.com>
2023-09-06 22:28:45 +0530

    first commit

---
+++ src/pages/ChangePassword.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import Link from 'next/link';
+
+const ChangePassword = () => {
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Change Your Password
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="currentPassword" className="sr-only">
+                Current Password
+              </label>
+              <input
+                id="currentPassword"
+                name="currentPassword"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Current Password"
+              />
+            </div>
+            <div>
+              <label htmlFor="newPassword" className="sr-only">
+                New Password
+              </label>
+              <input
+                id="newPassword"
+                name="newPassword"
+                type="password"
+                autoComplete="new-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="New Password"
+              />
+            </div>
+            <div>
+              <label htmlFor="confirmNewPassword" className="sr-only">
+                Confirm New Password
+              </label>
+              <input
+                id="confirmNewPassword"
+                name="confirmNewPassword"
+                type="password"
+                autoComplete="new-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Confirm New Password"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Change Password
+            </button>
+          </div>
+        </form>
+        <div className="text-sm text-center">
+          <p className="text-gray-600">
+            Remember your password?{' '}
+            <Link href="/login"
+               className="font-medium text-indigo-600 hover:text-indigo-500">
+                Log in
+             
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default ChangePassword
